773b7802df72623cf5a7281de338ea033da5bcf0
alexisc13 <103052010+alexisc13@users.noreply.github.com>
2024-07-02 13:45:23 -0500

    Update Conexion.php
    conexion

--- Conect/Conexion.php
+++ Conect/Conexion.php
@@ -4,10 +4,10 @@ namespace Conect;
  date_default_timezone_set('America/Lima'); 
  class Conexion{
       
-        const HOST = 'localhost';
-        const USER = 'root';
-        const PASSWORD = '';
-        const BDNAME = 'bdsistema';
+        const HOST = '64.23.178.172';
+        const USER = 'eldoctor_factu';
+        const PASSWORD = 'R82g3uuZO9kzl';
+        const BDNAME = 'eldoctor_facturador';
     public static function conectar() {
        
         $link = new \PDO("mysql:host=".self::HOST."; dbname=".self::BDNAME.";",
@@ -16,3 +16,4 @@ namespace Conect;
         return $link;
     }
 }
+
